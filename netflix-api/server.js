07e50311b47978763a984297d45c66389b3464ca
123Aliean <thejaswinice21@gmail.com>
2024-03-15 20:52:00 +0530

    commits

---
+++ netflix-api/server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import router from "../netflix-api/routes/UserRoute.js"
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect("mongodb://127.0.0.1:27017/netflix",{
+   useNewUrlParser: true,
+   useUnifiedTopology: true, 
+})
+.then(()=>{
+    console.log("DB Connected");
+
+});
+
+app.use("/api/user", router);
+
+app.listen(5000, console.log("server started"));
